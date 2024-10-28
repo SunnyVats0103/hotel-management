83dfebaa60d3bc984c674d169d263aa25a82bc96
sv-newmac <ssunnyvvats@outlook.com>
2024-10-28 19:45:23 +0530

    Add Staff entity (Example of creating relation)
     - Add entity Staff in project.
     - Modify Hotel with list of staffs and mappedBy entity to create relation with primary key.

---
+++ src/main/java/com/dev/nistha/entity/Staff.java
@@ -0,0 +1,34 @@
+package com.dev.nistha.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Staff {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(columnDefinition = "varchar(50)", nullable = false)
+    private String firstName;
+    @Column(columnDefinition = "varchar(50)", nullable = false)
+    private String lastName;
+    @Column(columnDefinition = "varchar(50)", nullable = false)
+    private String position;
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal salary;
+    @Column(columnDefinition = "date", nullable = false)
+    private Date dateOfBirth;
+    @Column(columnDefinition = "varchar(15)", nullable = false)
+    private String phone;
+    @Column(columnDefinition = "varchar(255)")
+    private String email;
+    @CreatedDate
+    private Date hireDate;
+    @ManyToOne
+    private Hotel hotel;
+
+}
