2ecb90e642b88dd9c1439956b06dfdf479b419c9
sv-newmac <ssunnyvvats@outlook.com>
2024-10-30 19:22:43 +0530

    Add Staff controller to project
     - Add StaffController class with /staff as url.
     - Modify Staff entity to include getter/setters.

---
+++ src/main/java/com/dev/nistha/controller/StaffController.java
@@ -0,0 +1,10 @@
+package com.dev.nistha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/staff")
+public class StaffController {
+
+}
