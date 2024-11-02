0d94f3562a3fe8a4fb268eaf62449806482fa84c
sv-newmac <ssunnyvvats@outlook.com>
2024-11-02 18:50:39 +0530

    Add changes to sync HotelController to other branches.

--- src/main/java/com/dev/nistha/controller/HotelController.java
+++ src/main/java/com/dev/nistha/controller/HotelController.java
@@ -1,34 +1,18 @@
 package com.dev.nistha.controller;
 
-import com.dev.nistha.dto.ResponseDTO;
-import com.dev.nistha.entity.Hotel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
-    
+
 //    Added for testing Git hub rules sets
 
-    @GetMapping
-    public ResponseEntity<Void> getHotels() {
-        return null;
-    }
-
-    @GetMapping("/{hotelId}")
-    public ResponseEntity<Void> getHotel(@PathVariable Long hotelId) {
-        return null;
-    }
-
-    @PostMapping
-    public ResponseEntity<ResponseDTO> createHotel(@RequestBody Hotel hotel) {
-        return null;
-    }
-
-    @DeleteMapping("/{hotelId}")
-    public ResponseEntity<ResponseDTO> deleteHotel(@PathVariable Long hotelId) {
-        return null;
+    @GetMapping("/detail")
+    public String getHotel() {
+        return "Raddison Blu";
     }
 
 }
