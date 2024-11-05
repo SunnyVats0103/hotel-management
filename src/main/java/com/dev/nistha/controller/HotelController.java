9b3781d5a2ac283e451c8dfd2f65a9eef81fe2b8
sv-newmac <ssunnyvvats@outlook.com>
2024-11-05 19:58:01 +0530

    Refactor files formatting.

--- src/main/java/com/dev/nistha/controller/HotelController.java
+++ src/main/java/com/dev/nistha/controller/HotelController.java
@@ -8,67 +8,70 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
-private final HotelService hotelService;
-        @PostMapping
-        public ResponseEntity<ResponseDTO> addNewHotel(@RequestBody HotelDTO hotelDTO) {
-            HotelDTO hotel = hotelService.createHotel(hotelDTO);
+    private final HotelService hotelService;
 
-            if (hotel != null) {
-                return ResponseEntity.status(HttpStatus.CREATED)
-                        .body(ResponseDTO.builder()
-                                .code("201")
-                                .message("Hotel Created")
-                                .build());
-            }
+    @PostMapping
+    public ResponseEntity<ResponseDTO> addNewHotel(@RequestBody HotelDTO hotelDTO) {
+        HotelDTO hotel = hotelService.createHotel(hotelDTO);
 
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        if (hotel != null) {
+            return ResponseEntity.status(HttpStatus.CREATED)
                     .body(ResponseDTO.builder()
-                            .code("500")
-                            .message("Hotel Creation Failed")
+                            .code("201")
+                            .message("Hotel Created")
                             .build());
         }
 
-        @GetMapping
-        public ResponseEntity<List<HotelDTO>> getAllHotel() {
-            return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotels());
-        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseDTO.builder()
+                        .code("500")
+                        .message("Hotel Creation Failed")
+                        .build());
+    }
 
-        @GetMapping("/{hotelId}")
-        public ResponseEntity<HotelDTO> getHotelById(@PathVariable Long hotelId) throws NoHotelFoundException {
-            return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotelById(hotelId));
-        }
+    @GetMapping
+    public ResponseEntity<List<HotelDTO>> getAllHotel() {
+        return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotels());
+    }
+
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<HotelDTO> getHotelById(@PathVariable Long hotelId) throws NoHotelFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotelById(hotelId));
+    }
 
 
-        @PutMapping
-        public ResponseEntity<ResponseDTO> updateHotel(@RequestBody HotelDTO hotelDTO) throws NoHotelFoundException {
-            HotelDTO hotel = hotelService.updateHotel(hotelDTO);
-            if (hotel != null) {
-                ResponseEntity.status(HttpStatus.OK)
-                        .body(ResponseDTO.builder()
-                                .code("200")
-                                .message("Hotel Updated")
-                                .build());
-            }
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ResponseDTO.builder()
-                            .code("500")
-                            .message("Hotel Update Failed")
-                            .build());
-        }
-
-        @DeleteMapping("/{hotelId}")
-        public ResponseEntity<ResponseDTO> deleteHotel(@PathVariable Long hotelId) {
-            hotelService.deleteHotel(hotelId);
-            return ResponseEntity.ok()
+    @PutMapping
+    public ResponseEntity<ResponseDTO> updateHotel(@RequestBody HotelDTO hotelDTO) throws NoHotelFoundException {
+        HotelDTO hotel = hotelService.updateHotel(hotelDTO);
+        if (hotel != null) {
+            ResponseEntity.status(HttpStatus.OK)
                     .body(ResponseDTO.builder()
                             .code("200")
-                            .message("Hotel Deleted")
+                            .message("Hotel Updated")
                             .build());
         }
-
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseDTO.builder()
+                        .code("500")
+                        .message("Hotel Update Failed")
+                        .build());
     }
+
+    @DeleteMapping("/{hotelId}")
+    public ResponseEntity<ResponseDTO> deleteHotel(@PathVariable Long hotelId) {
+        hotelService.deleteHotel(hotelId);
+        return ResponseEntity.ok()
+                .body(ResponseDTO.builder()
+                        .code("200")
+                        .message("Hotel Deleted")
+                        .build());
+    }
+
+}
--- src/main/java/com/dev/nistha/service/jpa/HotelServiceImpl.java
+++ src/main/java/com/dev/nistha/service/jpa/HotelServiceImpl.java
@@ -9,54 +9,55 @@ import com.dev.nistha.service.HotelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
-    @RequiredArgsConstructor
-    @Service
-    public class HotelServiceImpl implements HotelService {
+@RequiredArgsConstructor
+@Service
+public class HotelServiceImpl implements HotelService {
 
-        private final HotelRepository hotelRepository;
-        private final HotelMapper hotelMapper;
+    private final HotelRepository hotelRepository;
+    private final HotelMapper hotelMapper;
 
-        @Override
-        public HotelDTO getHotelById(Long id) throws NoHotelFoundException {
-            Optional<Hotel> savedHotel = hotelRepository.findById(id);
+    @Override
+    public HotelDTO getHotelById(Long id) throws NoHotelFoundException {
+        Optional<Hotel> savedHotel = hotelRepository.findById(id);
 
-            if (savedHotel.isEmpty())
-                throw new NoHotelFoundException("No hotel found with id: " + id);
+        if (savedHotel.isEmpty())
+            throw new NoHotelFoundException("No hotel found with id: " + id);
 
-            return hotelMapper.hotelToHotelDTO(savedHotel.get());
-        }
-
-        @Override
-        public List<HotelDTO> getHotels() {
-            List<Hotel> savedHotels = hotelRepository.findAllHotel();
-            return savedHotels.stream().map(hotelMapper::hotelToHotelDTO).toList();
-        }
-
-        @Override
-        @Transactional
-        public HotelDTO createHotel(HotelDTO hotelDTO) {
-            Hotel savedHotel = hotelRepository.save(hotelMapper.hotelDTOToHotel(hotelDTO));
-            return hotelMapper.hotelToHotelDTO(savedHotel);
-        }
-
-        @Override
-        @Transactional
-        public HotelDTO updateHotel(HotelDTO hotelDTO) throws NoHotelFoundException {
-            Optional<Hotel> existingHotel = hotelRepository.findById(hotelDTO.getId());
-            if (existingHotel.isEmpty())
-                throw new NoHotelFoundException("No hotel found with hotel number: " + hotelDTO.getId());
-
-            Hotel updatedHotel = hotelRepository.save(hotelMapper.hotelDTOToHotel(hotelDTO));
-            return hotelMapper.hotelToHotelDTO(updatedHotel);
-        }
-
-        @Override
-        @Transactional
-        public void deleteHotel(Long id) {
-            hotelRepository.deleteById(id);
-        }
-
+        return hotelMapper.hotelToHotelDTO(savedHotel.get());
     }
+
+    @Override
+    public List<HotelDTO> getHotels() {
+        List<Hotel> savedHotels = hotelRepository.findAllHotel();
+        return savedHotels.stream().map(hotelMapper::hotelToHotelDTO).toList();
+    }
+
+    @Override
+    @Transactional
+    public HotelDTO createHotel(HotelDTO hotelDTO) {
+        Hotel savedHotel = hotelRepository.save(hotelMapper.hotelDTOToHotel(hotelDTO));
+        return hotelMapper.hotelToHotelDTO(savedHotel);
+    }
+
+    @Override
+    @Transactional
+    public HotelDTO updateHotel(HotelDTO hotelDTO) throws NoHotelFoundException {
+        Optional<Hotel> existingHotel = hotelRepository.findById(hotelDTO.getId());
+        if (existingHotel.isEmpty())
+            throw new NoHotelFoundException("No hotel found with hotel number: " + hotelDTO.getId());
+
+        Hotel updatedHotel = hotelRepository.save(hotelMapper.hotelDTOToHotel(hotelDTO));
+        return hotelMapper.hotelToHotelDTO(updatedHotel);
+    }
+
+    @Override
+    @Transactional
+    public void deleteHotel(Long id) {
+        hotelRepository.deleteById(id);
+    }
+
+}
